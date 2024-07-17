a1d6e0baf0b728e29ef0605d722f8165269d4148
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-07-17 16:33:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+
+const inputs=document.querySelectorAll(".code");
+
+document.addEventListener("DOMContentLoaded",()=>{
+	inputs[0].focus();
+	
+});
+inputs.foreach((input,index) =>{
+	input.addEventListener("input",(event) => {
+    // If the input is a digit, move focus to the next input
+    if (/^\d$/.test(event.target.value) && index < inputs.length - 1) {
+      inputs[index + 1].focus();
+    }
+	input.addEventListener("keydown",(e) =>{
+		if(e.key==="Backspace"  && e.target.value===""  && index>0){
+			inputs[index-1].focus();
+		}
+	})
+		
+});
+	
